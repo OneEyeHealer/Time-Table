a8fead95fce66a03763392b523885a1c8b635aec
chiefking <chiefking@developer.no-reply.github.com>
2020-06-15 17:59:54 +0530

    Time Table demo

---
+++ resources/js/script.js
@@ -0,0 +1,176 @@
+$(document).ready(function(){
+
+  $(".image-popup").magnificPopup({
+  		type: 'image'
+  	});
+    $(".popup-link-1").magnificPopup({
+      removalDelay: 300,
+      // mainClass: 'mfp-fade',
+      type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
+    });
+
+/*---Wednesday time table  dialog box js-----*/
+  //cse-4
+    $(".cse-4-wednesday-3").magnificPopup({
+      removalDelay: 300,
+      // mainClass: 'mfp-fade',
+      type: 'inline',
+
+  		fixedContentPos: false,
+  		fixedBgPos: true,
+
+  		overflowY: 'auto',
+
+  		closeBtnInside: true,
+  		preloader: false,
+
+  		midClick: true,
+  		mainClass: 'mfp-fade'
+    });
+
+  // cse-6
+    $(".cse-6-wednesday-3").magnificPopup({
+      removalDelay: 300,
+      type: 'inline',
+
+      fixedContentPos: false,
+      fixedBgPos: true,
+
+      overflowY: 'auto',
+
+      closeBtnInside: true,
+      preloader: false,
+
+      midClick: true,
+      mainClass: 'mfp-fade'
+    });
+
+    /*---project Third dialog box js-----*/
+    $(".mca-2-wednesday-3").magnificPopup({
+      removalDelay: 300,
+      type:'inline',
+
+      // fixedContentPos: true,
+      // fixedBgPos: false,
+      //
+      // overflowY: 'auto',
+      // //
+      // // closeBtnInside: true,
+      // // preloader: false,
+      // //
+      // // midClick: true,
+      mainClass: 'mfp-fade'
+    });
+
+    /*------ Monday time table box for cse----------*/
+    // cse-4
+    $(".cse-4-monday-1").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    // cse-6
+    $(".cse-6-monday-1").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    // mca-2
+    $(".mca-4-monday-1").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+/*---tuesday dialog box of time table-----*/
+    $(".cse-4-tuesday-2").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".cse-6-tuesday-2").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".mca-2-tuesday-2").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    /*---Wednesday dialog box of time table-----*/
+    $(".cse-4-wednesday-3").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".cse-6-wednesday-3").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".mca-2-wednesday-3").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    /*---Thrusday dialog box of time table-----*/
+    $(".cse-4-thrusday-4").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".cse-6-thrusday-4").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".mca-2-thrusday-4").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    /*---Friday dialog box of time table-----*/
+    $(".cse-4-friday-5").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".cse-6-friday-5").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+    $(".mca-2-friday-5").magnificPopup({
+      type: 'inline',
+      removalDelay:300,
+      mainClass: 'mfp-fade'
+    });
+
+
+});
